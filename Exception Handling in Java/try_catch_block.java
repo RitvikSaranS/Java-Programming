6d58fb0ea7a980b3ebe1382c22510f00a1db2853
Ritvik Saran S <codebyritvik@gmail.com>
2022-04-01 09:44:28 +0530

    java programming

--- Exception Handling in Java/try_catch_block.java
+++ Exception Handling in Java/try_catch_block.java
@@ -12,7 +12,24 @@ public class try_catch_block {
             System.out.println("cannot divide by zero");
             System.out.println(e);
         }finally{
+            demo(5, 2);
             return 0;
         }
     }
+    //Nested try catch
+    public static void demo(int a, int b){
+        try{
+            System.out.println(8/a);
+            try{
+                int [] arr = new int[0];
+                System.out.println(arr[b]);
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
+        }catch(ArithmeticException e){
+            System.out.println(e);
+        }
+    }
 }
+
+
